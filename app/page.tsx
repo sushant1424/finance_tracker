32277f7932c394ad50a0e734c842dd6b6c5f9343
Sushant <sushantshrestha184@gmail.com>
2025-10-05 14:56:41 +0545

    Make a header section with login and signup functionality

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant="destructive">Click me</Button>
+    <div>
+      
+    </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,66 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { LayoutDashboard, PenBox } from "lucide-react"
 
 
 const Header = () => {
   return (
-    <div className = "fixed top-0">
-        <SignedOut>
-            <SignInButton />
-            <SignUpButton>
-             <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-             </button>
-            </SignUpButton>
-        </SignedOut>
+    <div className = "fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="flex items-center justify-between container mx-auto px-2 py-4">
+        <Link href="/">
+            <Image
+              src={"/image.png"}
+              alt="Welth Logo"
+              width={200}
+              height={60}
+              className="h-12 w-auto object-contain"
+            />
+        </Link>
 
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
+        <div className="flex items-center space-x-4">
+            
+            {/*User Actions After Login*/}
+            <SignedIn>
+              <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
+                <Button variant="outline">
+                  <LayoutDashboard size={18}/>  
+                  <span className="hidden md:inline">Dashboard</span>
+                </Button>
+              </Link>
+
+              <Link href={"/transaction/create"} >
+                <Button className="flex items-center gap-2">
+                  <PenBox size={18}/>  
+                  <span className="hidden md:inline">Create Transaction</span>
+                </Button>
+              </Link>
+            </SignedIn>
+
+
+            {/* User Sign in and Sign out buttons*/}
+            <SignedOut>
+                <SignInButton forceRedirectUrl={"/dashboard"}>
+                    <Button variant="outline">Log In</Button>
+                </SignInButton>
+                <SignUpButton forceRedirectUrl={"/dashboard"}>
+                    <Button variant="default">Sign Up</Button>
+                </SignUpButton>
+            </SignedOut>
+
+            <SignedIn>
+                <UserButton
+                   appearance={{
+                    elements:{
+                      avatarBox: "w-8 h-8 md:w-10 md:h-10",
+                    },
+                   }}
+                />
+            </SignedIn>
+        </div>
+        
+      </nav>
     </div>
   )
 }
