ab91365ce04cec5095971f5925a9647d5e208bc5
Sushant <sushantshrestha184@gmail.com>
2025-11-18 16:17:15 +0545

    Add a page.tsx file for budget page

---
+++ app/(main)/budget/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Breadcrumb } from '@/components/breadcrumb'
+
+interface AccountLayoutProps {
+  children: React.ReactNode;
+}
+
+const BudgetLayout = ({ children }: AccountLayoutProps) => {
+  return (
+    <div className="space-y-6 pb-8">
+      <Breadcrumb />
+      
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 tracking-tight">
+            Budgets
+          </h1>
+          <p className="text-sm text-muted-foreground mt-1">
+            Manage your budgets
+          </p>
+        </div>
+      </div>
+      {children}
+    </div>
+  )
+}
+
+export default BudgetLayout
---
+++ app/(main)/budget/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+ const BudgetPage = async () => {
+
+  return (
+    <div className ="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        Budget Page
+      
+    </div>
+  )
+}
+
+export default BudgetPage
